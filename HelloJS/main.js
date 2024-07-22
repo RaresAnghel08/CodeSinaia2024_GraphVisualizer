d139e6c0fcae3fd671c0cd06cd3273e72be6f98a
RaresAnghel08 <raresanghel2008@gmail.com>
2024-07-22 14:35:11 +0300

    script

--- HelloJS/main.js
+++ HelloJS/main.js
@@ -1,18 +1,13 @@
 let hTextInput = document.getElementById("textInput");
 let hDivOutput = document.getElementById("divOutput");
 let hBtnRun = document.getElementById("btnRun");
-let hBtnApp = document.getElementById("btnRun");
 let hBtnClear = document.getElementById("btnClear");
-let hBtnTrans = document.getElementById("btnTrans");
-let hBtnPrim = document.getElementById("btnPrim");
 
-hBtnApp.addEventListener('click', (event) => {
+hBtnRun.addEventListener('click', (event) => {
     var crtOutput = hDivOutput.innerHTML;
     var crtInput = hTextInput.value;
-    var crtInput = hTextInput.value.replaceAll('\n','<br>');
     if (crtOutput !== '') {
         hDivOutput.innerHTML = `${crtOutput}<br>${crtInput}`;
-        hDivOutput.innerHTML = `${crtOutput}\n${crtInput}`;
     } else {
         hDivOutput.innerHTML = crtInput;
     }
@@ -29,18 +24,3 @@ hBtnApp.addEventListener('click', (event) => {
 hBtnClear.addEventListener('click', (event) => {
     hDivOutput.innerHTML = '';
 });
-hBtnTrans.addEventListener('click', (event) => {
-    hDivOutput.innerHTML=hTextInput.value.replaceAll('\n','<br>');
-    hTextInput.value='';
-});
-hBtnPrim.addEventListener('click',(event)=> {
-    let n=hTextInput.value;
-    let ok=1;
-    if(n<2) hDivOutput.innerHTML="Compus";
-    else {for (let i=2; i*i<=n&&ok==1; i++) {
-            if(n%i==0) ok=0;
-    }
-    if(ok==1) hDivOutput.innerHTML="Prim";
-    else hDivOutput.innerHTML="Compus";}
-}
-);
